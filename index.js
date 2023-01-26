ebb21c249788b3b78d4b6a32173edc87f14c7aad
Mystica <r.dukuzeyez@alustudent.com>
2023-01-26 14:19:33 +0200

    feat(user-auth): implement user authentication
    - regist the accunt
    - sign in with you account
    
    [delivers #14 #16]

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const mongoose= require('mongoose');
 const bodyParser= require('body-parser');
 
 require('dotenv/config');
+
+require('dotenv/config');
 // const imageUpload=require(express-fileupload)
 
 app.use(bodyParser.json());
@@ -20,6 +22,11 @@ app.use('/posts', postsRouter);
 const messagesRouter= require('./routes/message');
 
 app.use('/messages', messagesRouter);
+
+
+const userRouter= require('./routes/auth');
+
+app.use('/auth', userRouter);
 // //middlewares
 // app.use('/posts', postsRouter(req,res) =>{
 //     console.log('this is middleware running')
