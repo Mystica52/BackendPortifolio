7dbed435c03c976571bec8cd2b6a3fdaafc7f60a
Mystica <r.dukuzeyez@alustudent.com>
2023-01-30 18:34:26 +0200

    this is  user authentication

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,8 @@ const router = express.Router()
 
 const AuthController = require ('../controller/auth.js')
 
+
+
 router.post('/', AuthController.register)
 router.post('/login', AuthController.login)
 
